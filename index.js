fa35f8466657dd0c71bda6ebbb64d7b65026fe88
tvkeprof <wefousoul@gmail.com>
2024-12-02 17:15:22 +0800

    test

---
+++ index.js
@@ -0,0 +1,25 @@
+console.log("hello from js variables");
+
+
+let age = 15;
+
+console.log(age);
+
+age = 40;
+ console.log(age);
+
+const myname = "bat";
+console.log (myname);
+
+const adress = "mongolia";
+const phone = "99999999"
+
+age = 33;
+console.log(age);
+
+const yourname = "bull";
+console.log (yourname);
+console.log(typeof phone);
+
+
+console.log(yourname + " nertei " + age + " nastai " + adress + " amidardag " + " utas " + phone);
